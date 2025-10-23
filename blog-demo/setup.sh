8ffc9b668ce7d1d14c60459f17701c876cc51afe
ihxnan <1377537238@qq.com>
2025-10-23 12:36:35 +0800

    update

--- blog-demo/setup.sh
+++ blog-demo/setup.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 npm i
+
+npm install hexo-deployer-git --save
+
+git clone -b main https://github.com/anzhiyu-c/hexo-theme-anzhiyu.git themes/anzhiyu
+
+npm install hexo-renderer-pug hexo-renderer-stylus --save
+
+npm install hexo-generator-search --save
